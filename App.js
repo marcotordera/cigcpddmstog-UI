67ccd402a026e70cecdb32adf203af038901b983
Michael W Lu <michaelwenlu@gmail.com>
2023-07-21 12:53:39 -0400

    Add routing, login page

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
 import { GlobalContextProvider } from "./GlobalContext";
+import Login from "./components/LoginScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import Root from "./components/Root";
+import { Provider as PaperProvider } from "react-native-paper";
+import LoginScreen from "./components/LoginScreen";
+import HomeScreen from "./components/NemesisSelectionScreen";
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-    <GlobalContextProvider>
-      <Root />
-    </GlobalContextProvider>
-  );
+	return (
+		<PaperProvider>
+			<GlobalContextProvider>
+				<NavigationContainer>
+					<Stack.Navigator
+						initialRouteName="Login"
+						screenOptions={{ headerShown: false }}
+					>
+						<Stack.Screen name="Login" component={LoginScreen} />
+						<Stack.Screen name="Home" component={HomeScreen} />
+					</Stack.Navigator>
+				</NavigationContainer>
+			</GlobalContextProvider>
+		</PaperProvider>
+	);
 }
---
+++ components/NemesisSelectionScreen.jsx
@@ -0,0 +1,15 @@
+// HomeScreen.js
+
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text>Welcome to the Home Screen!</Text>
+      {/* Add your home screen content here */}
+    </View>
+  );
+};
+
+export default HomeScreen;
